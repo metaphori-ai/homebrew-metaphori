a02ae018111f1201830820b7ead9c4322f7bfb9c
Jasdeep Jaitla <jasdeep@scalabl3.com>
2025-07-21 11:15:04 -0700

    Update to v0.1.2 with S3 URLs and correct checksums

--- Formula/metaphori.rb
+++ Formula/metaphori.rb
@@ -1,22 +1,22 @@
 class Metaphori < Formula
   desc "AI prompt compression CLI - Compress More. Spend Less. Think Better."
   homepage "https://metaphori.dev"
-  version "0.1.0"
+  version "0.1.2"
   license "MIT"
 
   # Binary releases for different platforms
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/metaphori-com/metaphori-cli/releases/download/v#{version}/metaphori-#{version}-darwin-arm64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"  # TO BE UPDATED
+    url "https://metaphori-releases.s3.amazonaws.com/v#{version}/metaphori-#{version}-darwin-arm64.tar.gz"
+    sha256 "e70d3d8db1e45aa76cfa4cfde97b280c91696dfb4bb8f517f286f5c77384de28"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/metaphori-com/metaphori-cli/releases/download/v#{version}/metaphori-#{version}-darwin-amd64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"  # TO BE UPDATED
+    url "https://metaphori-releases.s3.amazonaws.com/v#{version}/metaphori-#{version}-darwin-amd64.tar.gz"
+    sha256 "1bbb521e2c566de25881f4ad262581e24d282594d23fb15246a9c3d3c334f3f0"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/metaphori-com/metaphori-cli/releases/download/v#{version}/metaphori-#{version}-linux-amd64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"  # TO BE UPDATED
+    url "https://metaphori-releases.s3.amazonaws.com/v#{version}/metaphori-#{version}-linux-amd64.tar.gz"
+    sha256 "8d7e881c35b5a8a97ba895d12d696274c38507abedb74ca4cd52268230b3dfe6"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/metaphori-com/metaphori-cli/releases/download/v#{version}/metaphori-#{version}-linux-arm64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"  # TO BE UPDATED
+    url "https://metaphori-releases.s3.amazonaws.com/v#{version}/metaphori-#{version}-linux-arm64.tar.gz"
+    sha256 "52d978b6d89f2ce09fb68119aef90e8ef393e7f96c0e1d8623790f82171dc5c2"
   end
 
   def install
